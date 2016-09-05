068d39f3091ec80e2e9d072e7c7f1bedaec14a7f
Knifesurge <carlos94563@gmail.com>
2016-09-05 10:57:26 -0400

    Adding executable file (Pokedex){

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
  */
 
 #include <iostream>
-#include <iostd>
 #include <cstring>
 
 using namespace std;
@@ -672,3 +671,4 @@ void printPoke(Poke *poke)
 	cout << "Name: " << poke->name << endl;
 	cout << "Type: " << poke->type << endl;
 	cout << "ID: " << poke->poke_id << endl;
+}
